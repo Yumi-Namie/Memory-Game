0aea0a3a90d39d28a5c11aa04769024de3b9501e
Yumi-Namie <rn.yumi@gmail.com>
2023-09-09 01:21:32 +0200

    main - cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,16 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(
-
-    {
-    allowedHeaders: [ 'content-type' ],
-		origin: 'https://memory-game-gamma-seven.vercel.app',
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'https://memory-game-gamma-seven.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    }
-
-  );
+  });
+  
   app.setGlobalPrefix('brascuba')
 
   //Config de Swagger
