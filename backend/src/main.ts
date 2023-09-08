d30405a48c8a5af76b28406796fcf0fd675e039b
Yumi-Namie <rn.yumi@gmail.com>
2023-09-09 00:35:21 +0200

    another origin vercel cor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
     {
     allowedHeaders: [ 'content-type' ],
-		origin: 'https://memory-game-rmswweu7n-yumi-namie.vercel.app',
+		origin: 'https://memory-game-gamma-seven.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     }
